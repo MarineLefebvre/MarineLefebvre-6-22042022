73b81eb7e0f97e24480b0a16830569c277dbedd3
Marine <tech-03@kerlan-info.fr>
2022-05-02 10:42:43 +0200

    mise en place des routes

---
+++ app.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const stuffRoutes = require('./routes/stuff');
+
+
+app.use(express.json());
+
+//connexion a la base mongo db
+mongoose.connect('mongodb+srv://root:root@cluster0.9zacl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//utilisation de cors pour utiliser les requete extérieur
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+//utilisation du router en ajoutant la base de l'url qui s'ajoutera devant les url défini dans routes/stuff.js
+app.use('/api/stuff', stuffRoutes);
+
+module.exports = app;
---
+++ routes/stuff.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const router = express.Router();
+
+const Thing = require('../models/thing');
+
+//Insérer un objet en base de données
+router.post('/', (req, res, next) => {
+    const thing = new Thing({
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    thing.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+//récupérer un objet spécifique
+router.get('/:id', (req, res, next) => {
+    Thing.findOne({
+        _id: req.params.id
+    }).then(
+        (thing) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+});
+
+//mettre à jour un objet spécifique
+router.put('/:id', (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    Thing.updateOne({_id: req.params.id}, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+//supprimer un objet spécifique
+router.delete('/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+//récupérer l'ensemble des objets en BDD
+router.get('/' +
+    '', (req, res, next) => {
+    Thing.find().then(
+        (things) => {
+            res.status(200).json(things);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+module.exports = router;
